25814eca8c6bfa442f3ae0708324677d05efd778
andrewstane451-cmd <andrewstane451@gmail.com>
2026-06-25 17:17:19 +0300

    Update page.tsx

--- app/otp/page.tsx
+++ app/otp/page.tsx
@@ -130,7 +130,7 @@ function OTPPageContent() {
     body: JSON.stringify({
       mobile: phone,
       response_type: "json",
-      sender_name: "FULL_CIRCLE",
+      sender_name: "FULL_CIRC",
       service_id: 0,
       message,
     }),
